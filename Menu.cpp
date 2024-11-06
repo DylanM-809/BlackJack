2cf8bf48f031ba3d41414a5e6fc2b56dbc79d415
Ryice <rylanwillis05@gmail.com>
2024-11-05 21:55:43 -0900

    Functions and setup for menu file.

--- Menu.cpp
+++ Menu.cpp
@@ -1,12 +1,31 @@
-
+/***************************************************
+Rylan Willis
+Game Menu
+Date Completed:
+Description: A program that creates an interactable
+menu for the blackjack game.
+***************************************************/
 
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	cout << "Menu" << endl;
-	cout << "Play" << endl;
+// Function Prototypes
 
-	return 0;
+
+int main ()
+{
+    
+
+    return 0;
 }
+
+// Function to display Game Menu.
+
+
+// Function to start game.
+
+
+// Function to show how to play.
+
+
+// Function to exit game.
