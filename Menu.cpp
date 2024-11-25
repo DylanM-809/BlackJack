a8655f023ad9eac5272fa2c964594b62881fa9ad
Ryice <rylanwillis05@gmail.com>
2024-11-24 15:54:48 -0900

    Finished Menu, Blackjack file with menu.

--- Menu.cpp
+++ Menu.cpp
@@ -17,9 +17,55 @@ void displayMenu();
 void startGame();
 void gameInstructions();
 void exitGame();
+void Menu(); // Prototype for the Menu function
 
 int main() 
 {
+    Menu(); // Call the Menu function
+    return 0;
+}
+
+// Function to display Game Menu.
+void displayMenu() {
+    cout << "===== gamble.exe =====\n";
+    cout << "1. Start Game\n";
+    cout << "2. How to play\n";
+    cout << "3. Exit Game\n";
+    cout << "=====================\n";
+    cout << "Enter your choice: ";
+}
+
+// Function to start game.
+void startGame() {
+    cout << "Starting a new game...\n";
+    // Add your game code here
+    cout << "The game has started!\n";
+}
+
+// Function to show how to play.
+void gameInstructions() {
+    cout << "Welcome to the game gamble.exe!\n";
+    cout << "1. This game is pretty much Blackjack with a few twists.\n";
+    cout << "2. The goal of this game is to get as close to 21 as possible without going over.\n";
+    cout << "3. Aces are worth 1 or 11 points, face cards are worth 10 points, and all other cards are worth their face value.\n";
+    cout << "4. On your turn, you can choose to \"Hit\" to draw a card or \"Stand\" to stop drawing cards.\n";
+    cout << "5. The dealer must draw until their total is 17 or higher.\n";
+    cout << "6. The player with the higher total wins, unless they bust by going over 21.\n";
+    cout << endl;
+    cout << "Good Luck and Have Fun Playing!\n";
+    cout << "\nPress Enter to return to the menu screen.";
+    cin.ignore();
+    cin.get();
+    cout << endl;
+}
+
+// Function to exit game.
+void exitGame() {
+    cout << "Exiting Game...\n";
+}
+
+// Function to display the menu and handle user choices.
+void Menu() {
     int choice;
     bool running = true;
 
@@ -52,45 +98,4 @@ int main()
                 break;
         }
     }
-
-    return 0;
-}
-
-// Function to display Game Menu.
-void displayMenu() {
-    cout << "===== gamble.exe =====\n";
-    cout << "1. Start Game\n";
-    cout << "2. How to play\n";
-    cout << "3. Exit Game\n";
-    cout << "=====================\n";
-    cout << "Enter your choice: ";
-}
-
-// Function to start game.
-void startGame() {
-    cout << "Starting a new game...\n";
-    // Add your game code here
-    cout << "The game has started!\n";
-}
-
-// Function to show how to play.
-void gameInstructions() {
-    cout << "Welcome to the game gamble.exe!\n";
-    cout << "1. This game is pretty much Blackjack with a few twists.\n";
-    cout << "2. The goal of this game is to get as close to 21 as possible without going over.\n";
-    cout << "3. Aces are worth 1 or 11 points, face cards are worth 10 points, and all other cards are worth their face value.\n";
-    cout << "4. On your turn, you can choose to \"Hit\" to draw a card or \"Stand\" to stop drawing cards.\n";
-    cout << "5. The dealer must draw until their total is 17 or higher.\n";
-    cout << "6. The player with the higher total wins, unless they bust by going over 21.\n";
-    cout << endl;
-    cout << "Good Luck and Have Fun Playing!\n";
-    cout <<"\nPress Enter to return to the menu screen.";
-    cin.ignore();
-    cin.get();
-    cout << endl;
-}
-
-// Function to exit game.
-void exitGame() {
-    cout << "Exiting Game...\n";
 }
